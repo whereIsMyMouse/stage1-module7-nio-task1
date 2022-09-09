80001d05a6c8c1dc4bc17380a75e7dc0a75e6fa0
shariy <sharii.margarita@gmail.com>
2022-09-09 18:50:58 +0300

    commit1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,27 @@
 package com.epam.mjc.nio;
-
-import java.io.File;
-
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Path path = Paths.get("src/main/resources/Profile.txt");
+        StringBuilder content = new StringBuilder();
+        try (Stream<String> lines = Files.lines(path)){
+            lines.forEach(s -> content.append(s).append("--"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String dataProfile = content.toString();
+        String[] dataProfileSeparate = dataProfile.split("--");
+        String name = dataProfileSeparate[0].replaceFirst("Name: ", "");
+        int age = Integer.parseInt(dataProfileSeparate[1].replaceFirst("Age: ", ""));
+        String email = dataProfileSeparate[2].replaceFirst("Email: ", "");
+        long phone = Long.parseLong(dataProfileSeparate[3].replaceFirst("Phone: ", ""));
+
+        return new Profile(name, age, email, phone);
     }
 }
